39362089688dc119385a6be32cfe08fc6657380e
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-12-21 21:00:43 +1200

    Add the accertinity ui

--- fatching-data/app/page.tsx
+++ fatching-data/app/page.tsx
@@ -1,9 +1,10 @@
+import ClientComponent from "@/components/ClientComponent";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div className="">
-    saad
+    <ClientComponent />
    </div>
   );
 }
--- fatching-data/components/ClientComponent.tsx
+++ fatching-data/components/ClientComponent.tsx
@@ -2,8 +2,19 @@
 import React, { useEffect, useState } from 'react';
 
 const ClientComponent = () => {
-    
-    const [ClientSideData, setClientSideData] = useState([]);
+    interface productData {
+        "id": number,
+        "title":string,
+        "price": number,
+        "description": string,
+        "category": string,
+        "image":string,
+        "rating": {
+        "rate": number,
+        "count": number
+        }
+        }
+    const [ClientSideData, setClientSideData] = useState<productData[]>([]);
     
       async function gatData(){
         const data = await fetch('https://fakestoreapi.com/products')
@@ -11,12 +22,13 @@ const ClientComponent = () => {
         console.log(dataJson)
         setClientSideData(dataJson)
        }
+
     useEffect(() => {
         gatData()
     }, []);
     
     return (
-        <div className='container py-4 px-2 lg:grid grid-col-3 md:grid-col-2 grid-col-1'>
+        <div className='container py-4 px-2 grid gap-4 lg:grid-col-3 md:grid-col-2 grid-col-1'>
             
             
         </div>
